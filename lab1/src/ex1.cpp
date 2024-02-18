ea1e866b81ed4b3dcdce18d293a8b8f641c4c728
tyrty23 <kombarov.dmitry@gmail.com>
2024-02-18 17:53:24 +0300

    помагите

--- lab1/src/ex1.cpp
+++ lab1/src/ex1.cpp
@@ -1,10 +1,10 @@
 
-#include < stdio.h >
+#include <stdio.h>
 #include <locale.h>
 int main (void)
 { 
    setlocale(LC_ALL,"russian");
-   const char *str = "ГРуппа: ПМ-32 \n Команда №2 \n Состав: Каранкевич M., Комбаров Д., Комольцева Д.";
+   const char *str = "Группа: ПМ-32 \nКоманда №2 \nСостав: Каранкевич M., Комбаров Д., Комольцева Д.";
    puts (str);
                
    return 0;
