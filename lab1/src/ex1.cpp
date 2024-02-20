5a7c87f52d9a7e3f30366c0c46a2dde720593ab0
tyrty23 <108882283+tyrty23@users.noreply.github.com>
2024-02-20 12:52:46 +0000

    диана добавь в репорт пж

--- lab1/src/ex1.cpp
+++ lab1/src/ex1.cpp
@@ -1,10 +1,13 @@
 
 #include <stdio.h>
+#include <cstring>
+
 #include <locale.h>
 int main (void)
 { 
    setlocale(LC_ALL,"russian");
-   const char *str = "Группа: ПМ-32 \nКоманда №2 \nСостав: Каранкевич M., Комбаров Д., Комольцева Д.";
+   char str[150];
+   strcpy(str,"Группа: ПМ-32 \nКоманда №2 \nСостав: Каранкевич M., Комбаров Д., Комольцева Д.");
    puts (str);
                
    return 0;
