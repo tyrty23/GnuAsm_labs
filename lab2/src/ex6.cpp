da76f53863cac970119e07190ba197b298e0c984
DianaK <115741678+Ibilis37@users.noreply.github.com>
2024-02-23 23:39:36 +0000

    ex6_finally

--- lab2/src/ex6.cpp
+++ lab2/src/ex6.cpp
@@ -3,38 +3,35 @@
 #include <bitset>
 
 void print64(void* p){
-    unsigned int a=*reinterpret_cast<unsigned int*>(p);
-    int b=*(reinterpret_cast<int *>(p));
-    float c=*(reinterpret_cast<float *>(p));
+    unsigned long long int a=*reinterpret_cast<unsigned long long int*>(p);
+    long long int b=*(reinterpret_cast<int long long*>(p));
     double d=*(reinterpret_cast<double *>(p));
-
-    std::bitset<32> a2{a};
-    std::bitset<32> b2{(unsigned )b};
+    std::bitset<64> a2{a};
+    std::bitset<64> b2{(unsigned)b};
 
     std::cout
     <<std::setw(17)<<std::hex<<a            //а
     <<std::setw(65)<<a2                     //б
-    <<std::setw(20)<<std::dec<<a             //в
-    <<std::setw(20)<<std::dec<<b             //е
-    <<std::setw(50)<<std::setprecision(4)<<std::fixed<<c          //ж
-    <<std::setw(25)<<std::setprecision(4)<<std::scientific<<c     //з
-     <<std::setw(25)<<std::setprecision(4)<<d
+    <<std::setw(22)<<std::dec<<a             //в
+    <<std::setw(22)<<std::dec<<b             //е 
+    <<std::setw(50)<<std::fixed<<d      //ж
+    <<std::setw(25)<<std::scientific<<d  
     <<std::endl;
 
 }
 
 int main(){
     std::cout<<"\n"
-    <<std::setw(9)<<"hex"           
+    <<std::setw(17)<<"hex"           
     <<std::setw(65)<<"binary"                    
-    <<std::setw(20)<<"udec"            
-    <<std::setw(20)<<"dec"           
+    <<std::setw(22)<<"udec"            
+    <<std::setw(22)<<"dec"           
     <<std::setw(50)<<"fixed"        
     <<std::setw(25)<<"exp"
     <<std::endl;
 
 
-    std::cout<<"\nМинимальное целое 32-битное значение без знака:"<<std::endl;
+    std::cout<<"\nМинимальное целое 64-битное значение без знака:"<<std::endl;
     unsigned int a=0;
     void* p=reinterpret_cast<void *>(&a);
     print64(p);
@@ -55,12 +52,12 @@ int main(){
     print64(p);
 
     std::cout<<"\nЦелочисленное значение х = 5:"<<std::endl;
-    int e=5;
+    long long int e=5;
     p=reinterpret_cast<void *>(&e);
     print64(p);
 
     std::cout<<"\nЦелочисленное значение y = -5:"<<std::endl;
-    int f=-5;
+    long long int f=-5;
     p=reinterpret_cast<void *>(&f);
     print64(p);
 
@@ -69,17 +66,17 @@ int main(){
     p=reinterpret_cast<void *>(&g);
     print64(p);
 
-    std::cout<<"\nfloat-значение х = 5:"<<std::endl;
-    float h=5;
+    std::cout<<"\ndouble-значение х = 5:"<<std::endl;
+    double h=5.0;
     p=reinterpret_cast<void *>(&h);
     print64(p);
 
-    std::cout<<"\nfloat-значение y = -5:"<<std::endl;
-    h=-5;
+    std::cout<<"\ndouble-значение y = -5:"<<std::endl;
+    h=-5.0;
     p=reinterpret_cast<void *>(&h);
     print64(p);
 
-    std::cout<<"\nfloat-значение z = 0xFF007100:"<<std::endl;
+    std::cout<<"\ndouble-значение z = 0xFF007100:"<<std::endl;
     h=0xFF007100;
     p=reinterpret_cast<void *>(&h);
     print64(p);
