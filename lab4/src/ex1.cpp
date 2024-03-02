222d8d605fc31f94221d2ba2afa1643d281f6674
tyrty23 <kombarov.dmitry@gmail.com>
2024-03-02 22:25:51 +0300

    v 0.2

--- lab4/src/ex1.cpp
+++ lab4/src/ex1.cpp
@@ -2,34 +2,43 @@
 #define N 5
 
 #define PRINT_i(arr,type)\
-    std::cout << "\n\n"#arr " in decimal: "; \
-    for (int i = 0; i < N; i++) { printf("%lld ", static_cast<long long>(arr[i])); } \
     std::cout << "\n"#arr " in hex:     "; \
     for (int i = 0; i < N; i++) { printf("%#0*llx ", (int)(sizeof(type)*2 + 2), static_cast<long long>(arr[i]));} \
     std::cout << std::endl;
 
 int main(){
-    short xs=0xFADE;
+    short xs=0xEADE;
     int xl = 0xADE1A1D;
     long long xq = 0xC1A551F1AB1E;
     short Ms[N]{xs,xs,xs,xs,xs};
     int Ml[N]{xl,xl,xl,xl,xl};
     long long Mq[N]{xq,xq,xq,xq,xq};
 
-
-
+    std::cout<<"\n\nshort"<<std::endl;
     PRINT_i(Ms,short);
-    short a = 16;
     asm(
-        "mov dl, %1\n\t"
-        //"mov %0, eax\n\t"
-        : "=a" (Ms[2]) 
-        : "r" (a)
+        "movw ax, 16;\n"
+        "movw %0, ax;\n"
+        : "=m" (Ms[2]) 
     );
     PRINT_i(Ms,short);
 
-
+    std::cout<<"\n\nint"<<std::endl;
     PRINT_i(Ml,int);
+    asm(
+       "mov eax, 16;\n"
+        "mov %0, eax;\n"
+        : "=m" (Ml[2]) 
+    );
+    PRINT_i(Ml,int);
+
+    std::cout<<"\n\nlong long"<<std::endl;
+    PRINT_i(Mq,long long);
+    asm(
+        "movq rax, 16;\n"
+        "movq %0, rax;\n"
+        : "=m" (Mq[2]) 
+    );
     PRINT_i(Mq,long long);
 
     return 0;
