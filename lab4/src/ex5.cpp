44480df56b2f6fbb11c3de7f6b0aefbbb82876d9
tyrty23 <kombarov.dmitry@gmail.com>
2024-03-04 02:28:34 +0300

    need help with ex5

---
+++ lab4/src/ex5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define N 5
+
+#define PRINT_i(arr, type) \
+    std::cout << "\n" #arr " in hex:     "; \
+    for (int i = 0; i < N; i++) { \
+        printf("%#0*llx ", (int)(sizeof(type) * 2 + 2), static_cast<long long>(arr[i])); \
+    } \
+    std::cout << std::endl;
+
+int main() {
+    int xl = 0xADE1A1D;
+    int Ml[N]{xl, xl, xl, xl, xl};
+    int x=8;
+    size_t index = 2;
+    int *base = Ml;
+    int * x_=&x;
+
+    std::cout << "\n\nint" << std::endl;
+    PRINT_i(Ml, int);
+
+    asm volatile(
+        "movl %2, %%ecx\n\t"
+        "movl %%ecx, (%0,%1,4)\n\t" 
+        : 
+        : "r"(base), "r"(index),"m"(x_)
+        : "eax", "ecx", "memory"
+    );
+
+    PRINT_i(Ml, int);
+
+    return 0;
+}
