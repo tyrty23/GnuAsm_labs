e88fc981793b53467b3e5510fd1d615c5eaedfb4
tyrty23 <kombarov.dmitry@gmail.com>
2024-03-05 00:25:17 +0300

    lab4 without ex5

--- lab4/src/ex8.cpp
+++ lab4/src/ex8.cpp
@@ -4,26 +4,18 @@
     std::cout << "\n\n"#arr " in decimal: "; \
     for (int i = 0; i < N; i++) { printf("%.2f ", arr[i]); } \
     std::cout<<std::endl;
+
 int main() {
-    bool AVX_bit;
-    asm(
-        "cpuid\n"
-        "test $(1 << 28), %%ecx\n"
-        : "=@ccnz"(AVX_bit)
-        : "a"(1)
-        : "ebx", "ecx", "edx", "cc"
-    );
-    std::cout<<"AVX_bit = "<<AVX_bit<<std::endl;
-    std::cout<<"AVX is available"<<std::endl;
+
 
     float x1 = -8.0f/5.0f;
     float Mfs[N]{x1,x1,x1,x1,x1};
     PRINT_f(Mfs,float);
-    float x=4.8;
+    int x=4;
     size_t index = 2;
     float *base = Mfs;
     asm(
-        "movss %2, %%xmm1\n\t"
+        "cvtsi2ss %2, %%xmm1\n\t"
         "movss %%xmm1, (%0,%1,4)\n\t" 
         : 
         : "r"(base), "r"(index),"m"(x)
